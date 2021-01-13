b1203c5bd962ce9852871c2390475240fbd88283
Gitesh2808 <65801503+Gitesh2808@users.noreply.github.com>
2021-01-13 21:33:58 +0530

    First C program

---
+++ Gitesh2808/addition.c
@@ -0,0 +1,10 @@
+include<stdio.h>
+include<stdlib.h>
+int main(){
+  int a, b, sum;
+  printf("Enter two numbers :\n");
+  scanf("%d %d",&a, &b);
+  sum = a + b;
+  printf("Sum of two numbers is ", sum);
+  return 0;
+}
